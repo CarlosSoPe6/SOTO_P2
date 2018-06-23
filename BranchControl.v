b26f47c165e2517ddb2e0de3d5619987c41e8342
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-22 19:00:31 -0500

    Branch Control module created

---
+++ BranchControl.v
@@ -0,0 +1,22 @@
+/******************************************************************
+* Description
+* This module performes the control for the mux branch 
+* Version:
+*	1.0
+* Author:
+*	Carlos Soto Pérez
+* email:
+*	carlos348@outlook.com
+* Date:
+*	22/06/2018
+******************************************************************/
+module BranchModule(
+    input BEQControl,
+    input BNEControl,
+    input ZeroSignal,
+    output BranchControlSignal  
+);
+
+
+
+endmodule // BranchModule
