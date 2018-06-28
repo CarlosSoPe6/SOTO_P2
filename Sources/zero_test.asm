23ab4e41f7aa4cc4d1181434300ff96b6154b1c5
Salvador CG <ie708498@iteso.mx>
2018-06-27 19:51:40 -0500

    lal

---
+++ Sources/zero_test.asm
@@ -0,0 +1,13 @@
+
+.text
+main:	
+        addi    $sp, $zero, 0x10010200  #Adjusting stack pointer
+        add     $t1, $zero, 0x10010000
+for: 
+	beq	$t1, $sp, endfor	# if $t1 == $t0 then endfor
+	sw 	$zero, 0($t1)         # Loading data from memory (vector1)
+	addi	$t1, $t1, 4			# $t0 = $t0 + 4
+	j 	for
+endfor:
+      
+      add $zero, $zero, $zero 
