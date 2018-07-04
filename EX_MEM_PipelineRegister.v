5d154eb870155f4703271235a50de950d6a9f10a
Carlos Soto Pérez <carlos348@outlook.com>
2018-07-03 19:52:37 -0500

    Create ex mem pipeline register

---
+++ EX_MEM_PipelineRegister.v
@@ -0,0 +1,13 @@
+module EX_MEM_PipelineRegister(
+    input clk,
+    input reset,
+    input in_Zero,
+    input in_ALUResult,
+    input in_ReadData2,
+
+    output out_Zero,
+    output out_ALUResult,
+    output out_ReadData2
+);
+
+endmodule // EX_MEM_PipelineRegister
