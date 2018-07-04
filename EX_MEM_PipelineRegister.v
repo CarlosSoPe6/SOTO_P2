4464e122c32f9824cfb9600ea5d457e72c32896f
Carlos Soto Pérez <carlos348@outlook.com>
2018-07-04 10:46:28 -0500

    EX_MEM_PipelineRegister ready

--- EX_MEM_PipelineRegister.v
+++ EX_MEM_PipelineRegister.v
@@ -1,26 +1,43 @@
+/******************************************************************
+* Description
+*	
+* Version:
+*	1.0
+* Author:
+*	Carlos Soto Pérez
+* email:
+*	carlos348@outook.com
+* Date:
+*	03/07/2018
+******************************************************************/
 module EX_MEM_PipelineRegister(
     input clk,
     input reset,
+
     input in_Zero,
-    input in_ALUResult,
-    input in_ReadData2,
-    input in_JumpAddress,
-    input in_BranchAddress,
-    input in_PC_4,
+    input [31:0] in_ALUResult,
+    input [31:0] in_ReadData2,
+    input [31:0] in_NewPC,
+    input [31:0] in_PC_4,
+    input in_CtrlMemRead,
+    input in_CtrlMemWrite,
+    input in_CtrlALUOrMem,
 
     output out_Zero,
-    output out_ALUResult,
-    output out_ReadData2,
-    output out_JumpAddress,
-    output out_BranchAddress,
-    output out_PC_4
+    output [31:0] out_ALUResult,
+    output [31:0] out_ReadData2,
+    output [31:0] out_NewPC,
+    output out_CtrlMemRead,
+    output out_CtrlMemWrite,
+    output out_CtrlALUOrMem
 );
     reg Zero;
-    reg ALUResult;
-    reg ReadData2;
-    reg JumpAddress;
-    reg BranchAddress;
-    reg PC_4;
+    reg [31:0] ALUResult;
+    reg [31:0] ReadData2;
+    reg [31:0] NewPC;
+    reg CtrlMemRead;
+    reg CtrlMemWrite;
+    reg CtrlALUOrMem;
 
     always @(negedge reset or negedge clk) begin
       if(reset) 
@@ -28,26 +45,29 @@ module EX_MEM_PipelineRegister(
         Zero <= 0;
         ALUResult <= 0;
         ReadData2 <= 0;
-        JumpAddress <= 0;
-        BranchAddress <= 0;
-        PC_4 <= 0;
+        NewPC <= 0;
+        CtrlMemRead <= 0;
+        CtrlMemWrite <= 0;
+        CtrlALUOrMem <= 0;
       end 
       else 
       begin
         Zero <= in_Zero;
         ALUResult <= in_ALUResult;
         ReadData2 <= in_ReadData2;
-        JumpAddress <= in_JumpAddress;
-        BranchAddress <= in_BranchAddress;
-        PC_4 <= in_PC_4;
+        NewPC <= in_NewPC;
+        CtrlMemRead <= in_CtrlMemRead;
+        CtrlMemWrite <= in_CtrlMemWrite;
+        CtrlALUOrMem <= in_CtrlALUOrMem;
       end
     end
 
     assign out_Zero = Zero;
     assign out_ALUResult = ALUResult;
     assign out_ReadData2 = ReadData2;
-    assign out_JumpAddress = JumpAddress;
-    assign out_BranchAddress = BranchAddress;
-    assign out_PC_4 = PC_4;
+    assign out_NewPC = NewPC;
+    assign out_CtrlMemRead = CtrlMemRead;
+    assign out_CtrlMemWrite = CtrlMemWrite;
+    assign out_CtrlALUOrMem = CtrlALUOrMem;
 
 endmodule // EX_MEM_PipelineRegister
