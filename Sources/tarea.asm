fa8cef1cd14e71dad450528734a93b75d6b0e842
Carlos Soto Pérez <carlos348@outlook.com>
2018-07-05 10:31:35 -0500

    Tarea program

--- Sources/tarea.asm
+++ Sources/tarea.asm
@@ -6,3 +6,28 @@
 	add 	$zero, $zero, $zero
 	add 	$zero, $zero, $zero
 	add 	$t1, $zero, $t0
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	addi 	$t1, $t1, 2
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	addi 	$t2, $t1, 3
+	addi	$t3, $t3, 0x010010000
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	sw		$t2, 0($t3)
+	add		$s0, $t2, $t1
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	sub		$s1, $s0, $t3
+	lw		$t2, 0($t3)
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	addi	$s2, $t2, -2
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	or		$s2, $s2, $t4
+	add 	$zero, $zero, $zero
+	add 	$zero, $zero, $zero
+	sll		$s7, $s2, 2
+	
