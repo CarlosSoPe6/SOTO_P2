67c5f0cf16258da02430d897f4cb548bbe4310ad
Unknown <ie708498@iteso.mx>
2018-07-09 18:32:27 -0500

    Fixing Forwarding unit

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -8,33 +8,34 @@ module ForwardingUnit(
 	input [4:0] MEM_WB_RegisterRd,
 	
 	
-	output [1:0] ForwardA,
-	output [1:0] ForwardB
+	output reg [1:0] ForwardA,
+	output reg [1:0] ForwardB
 
 );
+
 
 always @*
 begin 
 
 	if(EX_MEM_RegWrite 
-     and (EX_MEM_RegisterRd != 0)  
-	  and (EX_MEM_RegisterRd = ID_EX_RegisterRs)) 
+     && (EX_MEM_RegisterRd != 0)  
+	  && (EX_MEM_RegisterRd == ID_EX_RegisterRs)) 
 	  ForwardA = 2'b10;
 	  
 	else if(MEM_WB_RegWrite 
-           and (MEM_WB_RegisterRd != 0)  
-			  and (EX_MEM_RegisterRd != ID_EX_RegisterRs))
-			  and (MEM_WB_RegisterRd = ID_EX_RegisterRs))  
+           && (MEM_WB_RegisterRd != 0)  
+			  && (EX_MEM_RegisterRd != ID_EX_RegisterRs)
+			  && (MEM_WB_RegisterRd == ID_EX_RegisterRs))  
 	  ForwardA = 2'b01;
 	  
-	else if((EX_MEM_RegWrite 
-            and (EX_MEM_RegisterRd != 0)  
-				and (EX_MEM_RegisterRd = ID_EX_RegisterRt))
+	else if(EX_MEM_RegWrite 
+            && (EX_MEM_RegisterRd != 0)  
+				&& (EX_MEM_RegisterRd == ID_EX_RegisterRt))
 	  ForwardB = 2'b10;
 	else if(MEM_WB_RegWrite 
-           and (MEM_WB_RegisterRd != 0)  
-			  and (EX_MEM_RegisterRd != ID_EX_RegisterRt))
-			  and (MEM_WB_RegisterRd = ID_EX_RegisterRt))
+           && (MEM_WB_RegisterRd != 0)  
+			  && (EX_MEM_RegisterRd != ID_EX_RegisterRt)
+			  && (MEM_WB_RegisterRd == ID_EX_RegisterRt))
 	  ForwardB = 2'b01;
    else 
 			begin
